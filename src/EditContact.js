7a9fa5f83b0613edb33651758456c9a06b406dad
pengfeitian0930 <90582201+pengfeitian0930@users.noreply.github.com>
2024-11-11 15:18:01 -0500

    Initial commit

---
+++ src/EditContact.js
@@ -0,0 +1,54 @@
+import { useState, useEffect, useCallback} from 'react';
+
+export default function EditContact(props) {
+  return (
+    <Editform 
+    {...props}
+    key={props.savedContact.id}
+    />
+  );
+    
+}
+
+function Editform({savedContact, onSave })  {
+  const [name, setName] = useState(savedContact.name);
+  const [email, setEmail] = useState(savedContact.email);
+
+  return (
+    <section >
+      <label>
+        Name:{' '}
+        <input
+          type="text"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+      </label>
+      <label>
+        Email:{' '}
+        <input
+          type="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+      </label>
+      <button onClick={() => {
+        const updatedData = {
+          id: savedContact.id,
+          name: name,
+          email: email
+        };
+        onSave(updatedData);
+      }}>
+        Save
+      </button>
+      <button onClick={() => {
+        setName(savedContact.name);
+        setEmail(savedContact.email);
+      }}>
+        Reset
+      </button>
+    </section>
+  );
+  
+}
